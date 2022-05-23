e31477ddea4d11b59a5e8dc549c2488799ddf597
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2022-05-23 18:34:29 +0700

    Update cloud-shell-export-scheduler.sh

--- cloud-shell-export-scheduler.sh
+++ cloud-shell-export-scheduler.sh
@@ -13,6 +13,7 @@ export SQL_SA=(`gcloud sql instances describe ${SQL_INSTANCE} \
     --project ${PROJECT_ID} \
     --format "value(serviceAccountEmailAddress)"`)
 
+# Create IAM Roles
 gcloud iam roles create ${STORAGE_ROLE} --project ${PROJECT_ID} \
     --title "Simple Storage Role" \
     --description "Grant permissions to view and create objects in Cloud Storage" \
@@ -23,8 +24,10 @@ gcloud iam roles create ${SQL_ROLE} --project ${PROJECT_ID} \
     --description "Grant permissions to export data from a Cloud SQL instance to a Cloud Storage bucket as a SQL dump or CSV file" \
     --permissions "cloudsql.instances.export"
 
+# Bind the simpleStorageRole role to Cloud Service Account
 gsutil iam ch serviceAccount:${SQL_SA}:projects/${PROJECT_ID}/roles/${STORAGE_ROLE} gs://${BUCKET_NAME}
 
+# Make a new Service Account for Cloud Function and bind the sqlExporter role
 gcloud iam service-accounts create ${GCF_NAME} \
     --display-name "Service Account for GCF and SQL Admin API"
 
@@ -32,6 +35,7 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member="serviceAccount:${GCF_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
     --role="projects/${PROJECT_ID}/roles/${SQL_ROLE}"
 
+# Create a new Pub/Sub topic
 gcloud pubsub topics create ${PUBSUB_TOPIC}
 
 # Make main.py file for Cloud Function
@@ -89,13 +93,19 @@ google-api-python-client
 Oauth2client
 EOF
 
+# Deploy main.py and requirements.txt to Cloud Functions with Python 3.9 runtime
 gcloud functions deploy ${GCF_NAME} \
     --trigger-topic ${PUBSUB_TOPIC} \
     --runtime python39 \
     --entry-point main \
     --service-account ${GCF_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
 
-gcloud scheduler jobs create pubsub ${SCHEDULER_JOB} --schedule '0 23 * * *' --topic ${PUBSUB_TOPIC} --message-body '{"db":"ecosense-db-sql","instance":'\"${SQL_INSTANCE}\"',"project":'\"${PROJECT_ID}\"',"gs":'\"gs://ecosense-db-sql-dump\"'}' --time-zone 'Asia/Jakarta'
+# Create a new Scheduler Job which sends message to Pub/Sub every 23.00
+gcloud scheduler jobs create pubsub ${SCHEDULER_JOB} \
+    --schedule '0 23 * * *' \
+    --topic ${PUBSUB_TOPIC} \
+    --message-body '{"db":"ecosense-db-sql","instance":'\"${SQL_INSTANCE}\"',"project":'\"${PROJECT_ID}\"',"gs":'\"gs://ecosense-db-sql-dump\"'}' \
+    --time-zone 'Asia/Jakarta'
 
 # Test
 gcloud scheduler jobs run ${SCHEDULER_JOB}
